7c40a2f4dca162a5eb431f607a5aeab95971fb74
袁佳乐 <geral@yuanjialedeMacBook-Air.local>
2024-11-28 17:08:50 +0800

    finish data analysis page

--- app/src/main/java/com/example/joyclean/AnalyzePage.kt
+++ app/src/main/java/com/example/joyclean/AnalyzePage.kt
@@ -7,8 +7,50 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.compose.material3.Button
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import com.example.joyclean.analyze.ProxyEnabledAppList
+import com.example.joyclean.database.AppManager
+import com.example.joyclean.setting.AppList
+
 @Composable
 fun AnalyzePage(navController: NavController) {
+
+    val context = LocalContext.current
+    val appManager = AppManager.getInstance(context)
+    var appListEnable by remember { mutableStateOf<List<String>>(emptyList()) }
+    var iconListEnable: List<ByteArray?> by remember { mutableStateOf<List<ByteArray?>>(emptyList()) }
+    var blockCountList: List<Int> by remember { mutableStateOf<List<Int>>(emptyList()) }
+    val refreshKey = remember { mutableIntStateOf(0) }
+    val seenAppNames = mutableSetOf<String>()
+
+    LaunchedEffect(refreshKey.value) {
+        appManager.getAppsWithProxyEnabledAsync { enabledApps ->
+            // 筛选出尚未处理的应用（通过 seenAppNames 去重）
+            val newApps = enabledApps.filter { it.appName !in seenAppNames }
+
+            // 筛选出图标不为 null 的应用
+            val filteredApps = newApps.filter { it.icon != null }
+
+            // 更新已见集合（确保应用唯一）
+            filteredApps.forEach { app -> seenAppNames.add(app.appName) }
+
+            // 构建一个 Map，以 appName 作为键，icon 作为值，去重
+            val appIconMap = (appListEnable.zip(iconListEnable) + filteredApps.map { it.appName to it.icon!! })
+                .toMap()
+
+            // 更新状态，确保去重
+            appListEnable = appIconMap.keys.toList()
+            iconListEnable = appIconMap.values.toList()
+            blockCountList = List(appListEnable.size) {0}
+        }
+    }
+
     PageLayout_Column(
         backgroundColor= Background_color.base_color,
         padding = ExtractPaddingValues(),
@@ -17,11 +59,6 @@ fun AnalyzePage(navController: NavController) {
         TopBarWithBackButton(title="数据分析",navController=navController)
 
         // 主体内容
-        Text(
-            text = "数据分析内容",
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
-        )
+        ProxyEnabledAppList(appList = appListEnable, iconList = iconListEnable, blockCountList = blockCountList, refreshKey=refreshKey)
     }
 }
